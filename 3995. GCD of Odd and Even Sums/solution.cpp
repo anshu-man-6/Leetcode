7c16c7b09b20dd6ae8f319808e21e1e9aa3d3f7b
anshu-man-6 <anshumanpandey8885@gmail.com>
2026-07-15 22:44:25 +0530

    Add solution for 3995. GCD of Odd and Even Sums

---
+++ 3995. GCD of Odd and Even Sums/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int sumodd=0;
+        int sumeven=0;
+
+        for(int i=1;i<=n*2;i++){
+            if(i%2==0){
+                sumeven+=i;
+            }else{
+              sumodd+=i;
+            }
+        }
+
+        return gcd(sumeven,sumodd);
+    }
+};

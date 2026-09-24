8f8ff1304182863e9427c151a7b7baac71f69990
anshu-man-6 <anshumanpandey8885@gmail.com>
2026-09-24 22:35:55 +0530

    Add solution for 200. Number of Islands

---
+++ 200. Number of Islands/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') {
+            return;
+        }
+        
+        grid[i][j] = '0'; // Mark as visited
+        
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.empty()) return 0;
+        
+        int count = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        
+        return count;
+    }
+};
